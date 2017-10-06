a420c241bf110fbb316b0180a45bd7fdf3f6a71a
Yu Morishige <mori-yu@nttpc.co.jp>
2017-10-06 01:22:25 +0000

    showアクション追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+100.times do |n|
+  title = ((0..9).to_a + ("a".."z").to_a + ("A".."Z").to_a).sample(5).join
+  content = ((0..9).to_a + ("a".."z").to_a + ("A".."Z").to_a).sample(5).join
+  Poem.create!(title: title,
+               content: content,
+               )
+end
